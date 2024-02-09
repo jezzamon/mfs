c299d8f2554f53e3baa6dbea98960bdc7e37819f
Jerry Relunia <jerryrelunia@kinsta.com>
2024-02-09 07:17:21 -0500

    testing echo

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi tttfdsfshere</h1>
+      <h1>Hi dere</h1>
       <hr />
       <MarketingApp />
     </div>
