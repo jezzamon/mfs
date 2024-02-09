3aae8ba96849be5fce2fcd187542f8a3c39df36e
Jerry Relunia <jerryrelunia@kinsta.com>
2024-02-09 09:32:07 -0500

    test

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
-
+const test = 'test';
 export default () => {
   return (
     <BrowserRouter>
