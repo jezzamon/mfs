06dd84babb8bb4c6a6348e4c64ecef7098c5df60
Jerry Relunia <jerryrelunia@kinsta.com>
2024-02-09 08:28:16 -0500

    add invalidation step to build for index.html

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi dere</h1>
+      <h1>Hi deree</h1>
       <hr />
       <MarketingApp />
     </div>
